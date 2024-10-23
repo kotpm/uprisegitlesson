9869e5b0ddd009551f9c645af9ee3154622a4ed4
kot4659448 <kotpm04@ukr.net>
2024-10-23 14:19:06 +0300

    labs2

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Ваш IP-адрес:</h1>
+    <p>{{ userIP }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const userIP = ref('Завантаження...');
+
+onMounted(() => {
+  fetch('https://api64.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      userIP.value = data.ip;
+    })
+    .catch(() => {
+      userIP.value = 'Не вдалося отримати IP';
+    });
+});
+</script>
+
+<style>
+
+</style>
